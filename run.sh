22fe3612834a59fdfffa446cd184cc3adb493e14
wjz <wjz2930608106@qq.com>
2024-11-05 14:53:46 +0800

    new

--- run.sh
+++ run.sh
@@ -7,7 +7,6 @@ python main.py --dataset stl10_2 --num-labeled 1000 --arch CNN_STL --batch-size 
 ###############################################################fmnist
 python main.py --dataset fmnist_1 --num-labeled 1000 --arch LeNet --batch-size 64 --lr 0.002  --out results/pn@fmnist1@1000
 python main.py --dataset fmnist_2 --num-labeled 1002 --arch LeNet --batch-size 64 --lr 0.002  --out results/pn@fmnist2@1000
-## credit card
 
 ########################################## alzheimer
 python main.py --dataset alzheimer --arch ResNet50 --batch-size 16 --lr 0.0005 --eval-step 100 --warming_steps 1000 --total-steps 2000 --out results/pn@alzheimer@769
